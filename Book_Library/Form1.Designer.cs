1a847247712664ce7e0eb433e42236298dc806e1
Roy Huang <roy.huang@issatek.com>
2018-10-12 18:15:25 +0800

    First_Ver

---
+++ Book_Library/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Book_Library
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.iSBNDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.authorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.publishinghouseDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.booklibraryBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.book_libDataSet = new Book_Library.book_libDataSet();
+            this.book_libraryTableAdapter = new Book_Library.book_libDataSetTableAdapters.book_libraryTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booklibraryBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.book_libDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iSBNDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.authorDataGridViewTextBoxColumn,
+            this.publishinghouseDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.booklibraryBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(442, 173);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // iSBNDataGridViewTextBoxColumn
+            // 
+            this.iSBNDataGridViewTextBoxColumn.DataPropertyName = "ISBN";
+            this.iSBNDataGridViewTextBoxColumn.HeaderText = "ISBN";
+            this.iSBNDataGridViewTextBoxColumn.Name = "iSBNDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // authorDataGridViewTextBoxColumn
+            // 
+            this.authorDataGridViewTextBoxColumn.DataPropertyName = "author";
+            this.authorDataGridViewTextBoxColumn.HeaderText = "author";
+            this.authorDataGridViewTextBoxColumn.Name = "authorDataGridViewTextBoxColumn";
+            // 
+            // publishinghouseDataGridViewTextBoxColumn
+            // 
+            this.publishinghouseDataGridViewTextBoxColumn.DataPropertyName = "publishing_house";
+            this.publishinghouseDataGridViewTextBoxColumn.HeaderText = "publishing_house";
+            this.publishinghouseDataGridViewTextBoxColumn.Name = "publishinghouseDataGridViewTextBoxColumn";
+            // 
+            // booklibraryBindingSource
+            // 
+            this.booklibraryBindingSource.DataMember = "book_library";
+            this.booklibraryBindingSource.DataSource = this.book_libDataSet;
+            // 
+            // book_libDataSet
+            // 
+            this.book_libDataSet.DataSetName = "book_libDataSet";
+            this.book_libDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // book_libraryTableAdapter
+            // 
+            this.book_libraryTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "update_btn";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 431);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booklibraryBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.book_libDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private book_libDataSet book_libDataSet;
+        private System.Windows.Forms.BindingSource booklibraryBindingSource;
+        private book_libDataSetTableAdapters.book_libraryTableAdapter book_libraryTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iSBNDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn authorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn publishinghouseDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Book_Library/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Book_Library
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'book_libDataSet.book_library' table. You can move, or remove it, as needed.
+            this.book_libraryTableAdapter.Fill(this.book_libDataSet.book_library);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int newRegionID = 5;     
+            try
+            {
+                this.book_libraryTableAdapter.Update(this.book_libDataSet.book_library);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Update Failed");
+            }
+            RefreshDataset();
+        }
+
+        private void RefreshDataset()
+        {
+            this.book_libraryTableAdapter.Fill(this.book_libDataSet.book_library);
+        }
+    }
+}
